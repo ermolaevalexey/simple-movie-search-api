6da66540d64de2af00238d77c20471c628e7209f
Alexey Ermolaev <aermolaev@griddynamics.com>
2019-11-06 19:47:40 +0300

    basic app

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log('Hello app');
+import * as Koa from 'koa';
+
+const app: Koa = new Koa();
+
+interface SumParams {
+    arg: string;
+}
+
+function sum(a: number): number {
+    return a + 2;
+}
+
+app.use((ctx: Koa.Context) => {
+    const queryArg: SumParams = ctx.query;
+    if (!queryArg.arg || isNaN(parseInt(queryArg.arg, 10))) {
+        ctx.status = 400;
+        ctx.body = 'Arg is not accepted';
+    } else {
+        ctx.body = `The sum is: ${sum(parseInt(queryArg.arg, 10))}`;
+    }
+});
+
+app.listen(3000);
