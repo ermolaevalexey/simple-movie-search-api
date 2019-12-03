27e9ee53196ce1d3d99f659eb7666ccc2762e695
Alexey Ermolaev <aermolaev@griddynamics.com>
2019-12-03 18:14:05 +0300

    fix db logging

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,7 +6,12 @@ const db = new Sequelize(
     process.env.DB_NAME as string,
     process.env.DB_USER as string,
     process.env.DB_PASSWORD,
-    { dialect: 'postgres' }
+    {
+        dialect: 'postgres',
+        logging: process.env.NODE_ENV === 'production'
+            ? undefined
+            : console.log
+    }
 );
 
 export default db;
