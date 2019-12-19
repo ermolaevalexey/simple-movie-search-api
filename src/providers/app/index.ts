7de61b9d734ef33802af51622b22c6888bbd2030
Alexey Ermolaev <aermolaev@griddynamics.com>
2019-12-19 12:15:28 +0300

    DEP_INJ di in progress - started decorators for routing implementation

--- src/providers/app/index.ts
+++ src/providers/app/index.ts
@@ -2,7 +2,6 @@ import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
 import * as Router from '@koa/router';
 import { Inject, Injectable } from '../../core/di';
-import { StoreProvider, TStoreProvider } from '../db';
 import { MoviesController } from '../movies/controller';
 
 export const TAppProvider = Symbol.for('AppProvider');
@@ -14,13 +13,7 @@ export class AppProvider {
         @Inject('Koa') private server: Koa,
         @Inject('Router') private router: Router,
         @Inject('MoviesController') private moviesController: MoviesController
-    ) {
-        this.router.get('/app', async (ctx, next) => {
-            ctx.body = JSON.stringify({
-                app: 'App is Running!'
-            });
-        })
-    }
+    ) {}
 
     run() {
         this.server
