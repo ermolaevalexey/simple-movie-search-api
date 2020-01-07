6ae925b366ccc7204e1867ed9e560c40357e454b
Alexey Ermolaev <aermolaev@griddynamics.com>
2020-01-07 13:22:14 +0100

    DEP_INJ fix app port to port

--- src/core/providers/env/index.ts
+++ src/core/providers/env/index.ts
@@ -4,7 +4,7 @@ import { Injectable } from '../../di';
 @Injectable()
 export default class EnvProvider {
     env = process.env.NODE_ENV as string;
-    appPort = parseInt(process.env.APP_PORT as string, 10);
+    appPort = parseInt(process.env.PORT as string, 10);
     store = {
         name: process.env.DB_NAME as string,
         user: process.env.DB_USER as string,
