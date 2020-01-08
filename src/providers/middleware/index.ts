c7fb2bb8428fccc7c4ee97bc0e1d8b4658bfb897
Alexey Ermolaev <aermolaev@griddynamics.com>
2020-01-08 20:13:54 +0100

    POSTERS - fixes - mkdir fixes

--- src/providers/middleware/index.ts
+++ src/providers/middleware/index.ts
@@ -54,7 +54,6 @@ export class MiddlewareProvider {
             } catch (err) {
                 ctx.status = 502;
                 ctx.headers['Content-Type'] = 'application/json';
-                console.error(err);
                 ctx.body = JSON.stringify({ error: err.message });
             }
         }
