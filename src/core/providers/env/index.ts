04320235942e923f593c794c14416b1732a3e3be
Alexey Ermolaev <aermolaev@griddynamics.com>
2020-03-23 18:51:27 -0700

    AUTH - creation of auth logic

--- src/core/providers/env/index.ts
+++ src/core/providers/env/index.ts
@@ -8,6 +8,7 @@ export default class EnvProvider {
     env = process.env.NODE_ENV as string;
     appPort = parseInt(process.env.PORT as string, 10);
     dbUrl = process.env.DATABASE_URL as string;
+    authSecret = process.env.AUTH_SECRET as string;
 
     mongoDbUrl = process.env.MONGO_DB_URL as string;
     mongoDbName = process.env.MONGO_DB_NAME as string;
